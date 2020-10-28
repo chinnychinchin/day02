1c7e411bdd9dc394e8d1dc061c8028676e3e7a10
chinyanlim <chinyan77@gmail.com>
2020-10-28 09:57:56 +0800

    My dice app

--- index.js
+++ index.js
@@ -17,10 +17,13 @@ app.set('view engine', 'hbs');
 
 //Configure the application
 
+/*Load/Mount static files. Note that the directory that you mount is the root directory.
+You can go into the directory but cannot go up the directory. 
+*/
 
-app.use(
-    express.static(__dirname + '/media/dice_images')
-)
+app.use(express.static(__dirname + '/media'))
+app.use(express.static(__dirname + '/static'))
+
 //Get /
 app.get('/', (req,res) => {
 
@@ -32,14 +35,43 @@ app.get('/', (req,res) => {
 
 app.get('/rollPage.hbs',(req,res) => {
 
-    let roll = Math.floor(Math.random())*6
-    console.log(roll)
+    let roll1 = Math.ceil(Math.random()*6)
+    let roll2 = Math.ceil(Math.random()*6)
+
+    const dieToShow = (roll) => {
+
+        switch(roll) {
+            case 1: return "dado-1.png"
+            break;
+            case 2: return "roll2.png"
+            break;
+            case 3: return "three_dots.png"
+            break;
+            case 4: return "four.png"
+            break;
+            case 5: return "Five-Image.png"
+            break;
+            case 6: return "dice-showing-6.png"
+            break;
+            default:
+                return "2_dice"
+    
+        }
+    }
+
+    let die1 = dieToShow(roll1);
+    let die2 = dieToShow(roll2);
     res.status(200);
     res.type('text/html');
-    res.render('rollPage')
+    res.render('rollPage',{die1,die2})
+})
+
+//to redirect the client back to landing page whenever a user puts some random noun
+app.use((req,res) => {
+    res.redirect('/')
 })
 
 
 
 //Start express
-app.listen(PORT, () => {console.log(`App started on port ${PORT}`)})
+app.listen(PORT, () => {console.log(`App started on port ${PORT} at ${new Date()}`)})
